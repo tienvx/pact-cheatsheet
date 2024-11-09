0cb8f7b4ef6f7745c4265881925655967af0bc8b
tien.xuan.vo <tien.xuan.vo@gmail.com>
2024-11-09 12:23:39 +0700

    refactor: Move diagram to correct place. Add plantuml installation guide

--- slides/pact/build.sh
+++ slides/pact/build.sh
@@ -7,4 +7,5 @@ docker run --rm -u `id -u`:`id -g` -v $dir/src/diagrams:/data -v $dir/src/media:
 docker run --rm -u `id -u`:`id -g` -v $dir/src/diagrams:/data -v $dir/src/media:/output minlag/mermaid-cli -i ci-frontend.mmd -o ../output/ci-frontend.svg
 docker run --rm -u `id -u`:`id -g` -v $dir/src/diagrams:/data -v $dir/src/media:/output minlag/mermaid-cli -i contract-test.mmd -o ../output/contract-test.svg
 docker run --rm -u `id -u`:`id -g` -v $dir/src/diagrams:/data -v $dir/src/media:/output minlag/mermaid-cli -i contract-verification.mmd -o ../output/contract-verification.svg
+cat $dir/src/diagrams/code-first-consumer-driven.puml | plantuml -pipe > $dir/src/media/code-first-consumer-driven.png
 mdslides --include $dir/src/media --output_dir $dir/../../output/pact $dir/src/presentation.md
